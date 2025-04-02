1e9fac90412f43f4f6bb74a57e469ceafe52fb22
giridharan2003 <giridharankarunakaran2003@gmail.com>
2025-04-02 11:13:57 +0530

    First Commit

---
+++ src/main/java/com/example/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Hello {
+	
+	@GetMapping("/")
+	public String getHello() {
+		return "Hello Welcome To Jenkins";
+	}
+	
+}
